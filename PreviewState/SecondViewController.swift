2866f6b99e9e844a8502d7e02f1e4fc8cf613188
FukuyamaShingo <houseshou@FukuyamaShingos-MacBook-Pro.local>
2016-02-24 02:55:57 +0900

    Add gif

--- PreviewState/SecondViewController.swift
+++ PreviewState/SecondViewController.swift
@@ -40,7 +40,7 @@ class SecondViewController: UIViewController {
             self.view.backgroundColor = UIColor(red: 0.3, green: 0.8, blue: 0.3, alpha: 1.0)
         }
         else {
-            indicator.text = "Not peeking anymore."
+            indicator.text = "Popped."
             self.view.backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 0.8, alpha: 1.0)
         }
     }
